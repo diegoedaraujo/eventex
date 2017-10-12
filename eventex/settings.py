deeb6364f65ab4fc46b67039babf7ca24e6bec25
Diego Araújo <diegoedaraujo@gmail.com>
2017-10-12 17:24:23 -0300

    Ocultando os hosts

--- eventex/settings.py
+++ eventex/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import config, Csv
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,10 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['eventex-diegoaraujo.herokuapp.com',
-                 '127.0.0.1',
-                 'localhost'
-                 ]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
